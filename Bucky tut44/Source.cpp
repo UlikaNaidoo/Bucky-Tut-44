f9d094da96db0d205aea79510db0fbd90146e36c
UlikaNaidoo <ulika.d.naidoo@gmail.com>
2016-03-22 21:43:55 +0200

    Yawn

---
+++ Bucky tut44/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "UD.h"
+
+using namespace std;
+
+int main() {
+	UD uobj;
+	uobj.printShiz();
+
+	const UD consobj;
+	consobj.printshiz2();
+
+	//Oh well
+}
---
+++ Bucky tut44/UD.h
@@ -0,0 +1,14 @@
+#ifndef UD_H
+#define UD_H
+
+class UD
+{
+public:
+	UD();
+	void printShiz();
+	void printshiz2() const;
+
+	~UD();
+};
+
+#endif
